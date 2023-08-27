1d3558a2a1f6038438d473aa61f61f1fe8fbba16
leandro-ikehara <leandro.ikehara@gmail.com>
2023-08-27 14:08:37 -0300

    Refatorando classe

--- extrator-url/main.py
+++ extrator-url/main.py
@@ -1,24 +1,20 @@
 # Fatiamento de strings
-
-# url = "bytebank.com/cambio?moedaDestino=dolarmoedaOrigem=real"
 url = "bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
 
+# Limpeza da URL
+url = url.strip() #remove espaços em branco
+
+# Validação da URL
+if url == "":
+    raise ValueError("A URl está vazia")
+
+# Separa base e parâmetros
 indice_interrogacao = url.find('?')
 url_base = url[:indice_interrogacao]
-
 url_parametros = url[indice_interrogacao+1:]
 print(url_parametros)
 
 # Busca o valor do parâmetro
-# parametro_busca = 'moedaOrigem'
-# indice_parametro = url_parametros.find(parametro_busca)
-#
-# print(indice_parametro)
-#
-# indice_valor = indice_parametro + len(parametro_busca) + 1
-# valor = url_parametros[indice_valor:]
-#
-# print(valor)
 parametro_busca = 'quantidade'
 indice_parametro = url_parametros.find(parametro_busca)
 indice_valor = indice_parametro + len(parametro_busca) + 1
